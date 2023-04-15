9743f671503b5eba47237d581633bb86b8ed879f
dale <hessdale96@gmail.com>
2023-04-15 17:15:29 -0600

    added variables for user content and date and used html to inject variables

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <display-posts></display-posts>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DisplayPosts from "@/components/DisplayPosts.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    DisplayPosts,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/DisplayPosts.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <article>
+      <h3>user: {{ user }}</h3>
+      <p>content: {{ content }}</p>
+      <h4>date created: {{ date_created }}</h4>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  //adding variables for the html to refer too
+  data() {
+    return {
+      user: `Dale`,
+      content: `lorem ipsum blah blah blah`,
+      date_created: `4 / 15 / 2023`,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
